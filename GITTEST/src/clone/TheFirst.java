a96833af641eeddfc731578fc2ce994349a88f4f
Administrator <Administrator@NGUY-6IQS7RAOEL>
2018-03-02 21:56:06 +0700

    OOL

--- GITTEST/src/clone/TheFirst.java
+++ GITTEST/src/clone/TheFirst.java
@@ -2,5 +2,12 @@ package clone;
 
 public class TheFirst {
 
-	
+	public static void main (String[] args)
+	{
+		System.out.println("Hello");
+		System.out.println();
+		System.out.println("Hello HNHNH");
+
+		
+	}
 }
